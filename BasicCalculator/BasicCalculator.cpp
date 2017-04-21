1968d9338450eda04d6ac62a6228d20cbf1e6d57
Tobias Andersson <siesta2007@hotmail.com>
2017-04-21 23:50:29 +0200

    Initial commit.

---
+++ BasicCalculator/BasicCalculator.cpp
@@ -0,0 +1,18 @@
+// BasicCalculator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <Windows.h>
+
+int main()
+{
+	printf("Welcome to my little calculator.\nWhat could I do for you today?\n\n");
+
+	Sleep(2000);
+
+	printf("Bai bai\n");
+	Sleep(2000);
+
+	return 0;
+}
+
